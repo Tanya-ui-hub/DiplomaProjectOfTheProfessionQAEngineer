37eccfc982725f4701844c9f9df2b1c7f91430f4
Tanya-ui-hub <tatyanapenzina92@gmail.com>
2021-10-15 09:51:03 +0300

    Добавлена папка pages

---
+++ src/test/java/ru/netology/pages/OfferPage.java
@@ -0,0 +1,26 @@
+package ru.netology.pages;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
+public class OfferPage {
+    private SelenideElement buyButton = $(byText("Купить"));
+    private SelenideElement creditButton = $(byText("Купить в кредит"));
+    private SelenideElement paymentByCard = $(byText("Оплата по карте"));
+    private SelenideElement paymentByCreditCard = $(byText("Кредит по данным карты"));
+
+    public PaymentPage payByDebitCard() {
+        buyButton.click();
+        paymentByCard.shouldHave(Condition.visible);
+        return new PaymentPage();
+    }
+
+    public PaymentPage payCreditByCard() {
+        creditButton.click();
+        paymentByCreditCard.shouldHave(Condition.visible);
+        return new PaymentPage();
+    }
+}
